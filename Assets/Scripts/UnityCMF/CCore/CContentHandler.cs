d53fbb745eda54f8583b57a730a939b846d7c4bf
Markus Scheidgen <markus.scheidgen@gmail.com>
2014-12-07 21:55:06 +0100

    Added a content adapter equivalent.

---
+++ Assets/Scripts/UnityCMF/CCore/CContentHandler.cs
@@ -0,0 +1,24 @@
+using UnityCMF.ECore;
+
+namespace UnityCMF.CCore
+{
+	public class CContentHandler
+	{
+		public event CHandleNotification CNotification;
+
+		protected virtual void HandleNotification(CAction action) {
+			if (action.Feature is EReference && (action.Feature as EReference).IsContainment) {
+				if (action.OldValue != null && action.OldValue is CObject) {
+					(action.OldValue as CObject).CNotification -= HandleNotification;
+				}
+				if (action.NewValue != null && action.NewValue is CObject) {
+					(action.NewValue as CObject).CNotification += HandleNotification;
+				}
+			}
+			if (CNotification != null) {
+				CNotification(action);
+			}
+		}
+	}
+}
+
